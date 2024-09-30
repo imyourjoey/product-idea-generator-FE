09aa0df1cfdf5911ad2dcf06bad7a784c3b18f46
imyourjoey <joeylim016@gmail.com>
2024-09-30 21:37:10 +0800

    add env files

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000", // Laravel backend URL
+  baseURL: process.env.VUE_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
