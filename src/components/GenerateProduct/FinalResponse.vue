48e74b2f8eda7145d0bdc949de9df8b50841880f
imyourjoey <joeylim016@gmail.com>
2024-10-03 11:43:38 +0800

    handled loading, format response text

---
+++ src/components/GenerateProduct/FinalResponse.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="text-3xl font-bold mt-3">Your Product Idea is Ready!</div>
+  <div
+    class="text-sm text-gray-500 mb-3 mt-2 text-right flex justify-end align-center"
+  >
+    - crafted with <IconHeartFill class="mx-1" width="14" /> by the Produck.ai
+    team
+  </div>
+
+  <div class="card">
+    <div class="text-gray-500">Product Name</div>
+    <div class="mb-4">{{ props.response.productName }}</div>
+
+    <div class="text-gray-500">Description</div>
+    <div class="mb-4">{{ props.response.description }}</div>
+
+    <div class="text-gray-500">Unique Selling Point</div>
+    <div class="mb-4">{{ props.response.uniqueSellingPoint }}</div>
+
+    <div class="text-gray-500">Target Market</div>
+    <div class="mb-4">{{ props.response.targetMarket }}</div>
+
+    <div class="text-gray-500">Estimated Manufacturing Cost (Per Unit)</div>
+    <div class="mb-4">USD {{ props.response.estimatedCost }}</div>
+
+    <div class="text-gray-500">Estimated Selling Price (Per Unit)</div>
+    <div class="mb-4">USD {{ props.response.estimatedSellingPrice }}</div>
+
+    <div class="text-gray-500">Estimated Units Sold Per Month</div>
+    <div class="mb-4">
+      {{ props.response.estimatedUnitsSoldPerMonth }} units
+    </div>
+
+    <div class="text-gray-500">Product Image</div>
+    <div class="mb-4" style="min-height: 100px">
+      <div v-if="isLoadingImage" class="flex justify-center items-center h-64">
+        <n-spin size="large">
+          <template #description>
+            <div class="text-center">
+              <div class="text-md text-[#8a2be1]">Hang in there...</div>
+              <div class="text-md text-[#8a2be1]">
+                We are generating your product image!
+              </div>
+            </div>
+          </template>
+        </n-spin>
+      </div>
+      <img
+        :src="imageUrl"
+        alt="Generating Image..."
+        class="rounded-xl"
+        @load="handleImageLoaded"
+        @error="handleImageError"
+      />
+    </div>
+  </div>
+
+  <n-button @click="reset" block type="primary" class="my-4"
+    >Generate a New Product Idea</n-button
+  >
+</template>
+
+<script setup>
+import { NButton, NSpin } from "naive-ui";
+import { defineProps, defineEmits, ref } from "vue";
+import IconHeartFill from "../../svgicons/IconHeartFill.vue";
+
+const emits = defineEmits("reset");
+
+const props = defineProps({
+  response: {
+    type: Object,
+    default: {},
+    required: false,
+  },
+  imageUrl: {
+    type: String,
+    default: "",
+    required: false,
+  },
+});
+
+function reset() {
+  emits("reset");
+}
+const isLoadingImage = ref(true);
+
+function handleImageLoaded() {
+  isLoadingImage.value = false;
+}
+
+function handleImageError() {
+  isLoadingImage.value = false;
+  console.error("Failed to load image");
+}
+</script>
--- src/components/GenerateProduct/Index.vue
+++ src/components/GenerateProduct/Index.vue
@@ -1,97 +1,90 @@
 <template>
-  {{ aiResponse }}
   <div class="md:w-1/2">
     <template v-if="!fullPromptResponse">
-      <div class="font-bold text-3xl my-4">
-        Unlock Your Next Product Idea with the Power of
-        <n-gradient-text
-          gradient="linear-gradient(90deg, #D68A3C, #A05FAE, #3BA2A9)"
-          class="font-bold"
-        >
-          produck.ai!</n-gradient-text
-        >
-      </div>
-      <div class="flex items-center">
-        <div class="text-lg font-bold">Use Existing Brand Profile</div>
-        <n-switch
-          v-model:value="useBrandProfile"
-          size="small"
-          class="ms-auto"
-        />
-      </div>
-      <n-collapse-transition :show="useBrandProfile">
-        <n-select
-          v-model:value="selectedBrand"
-          :options="brandOptions"
-          placeholder="Select a Brand"
-        />
-        <n-button type="primary" class="mt-2" @click="showModal">
-          Create New Brand
-        </n-button>
+      <n-spin :show="loading" size="large">
+        <template #description>
+          <div class="text-center">
+            <div class="text-md">Hang in there...</div>
+            <div class="text-md">We are crafting your product!</div>
+          </div>
+        </template>
+        <div class="font-bold text-3xl my-4">
+          Unlock Your Next Product Idea with the Power of
+          <n-gradient-text
+            gradient="linear-gradient(90deg, #D68A3C, #A05FAE, #3BA2A9)"
+            class="font-bold"
+          >
+            produck.ai!</n-gradient-text
+          >
+        </div>
+        <div class="flex items-center">
+          <div class="text-lg font-bold">Use Existing Brand Profile</div>
+          <n-switch
+            v-model:value="useBrandProfile"
+            size="small"
+            class="ms-auto"
+          />
+        </div>
+        <n-collapse-transition :show="useBrandProfile">
+          <n-select
+            v-model:value="selectedBrand"
+            :options="brandOptions"
+            placeholder="Select a Brand"
+          />
+          <n-button type="primary" class="mt-2" @click="showModal">
+            Create New Brand
+          </n-button>
 
-        <n-collapse-transition :show="selectedBrand != null">
-          <BrandProductsInfo :brand-products="brandProducts" class="mt-4" />
+          <n-collapse-transition :show="selectedBrand != null">
+            <BrandProductsInfo :brand-products="brandProducts" class="mt-4" />
+          </n-collapse-transition>
         </n-collapse-transition>
-      </n-collapse-transition>
-      <div class="mt-4">
-        <div class="text-lg font-bold">
-          Describe the Product You Want to Create!
+        <div class="mt-4">
+          <div class="text-lg font-bold">
+            Describe the Product You Want to Create!
+          </div>
+          <div class="text-gray-500">
+            Be brief and clear in your description. You can include features,
+            benefits, target audience, and any specific requirements.
+          </div>
+          <n-input
+            v-model:value="userPrompt"
+            type="textarea"
+            placeholder="Insert Prompt"
+          />
         </div>
-        <div class="text-gray-500">
-          Be brief and clear in your description. You can include features,
-          benefits, target audience, and any specific requirements.
-        </div>
-        <n-input
-          v-model:value="userPrompt"
-          type="textarea"
-          placeholder="Insert Prompt"
-        />
-      </div>
 
-      <div class="mt-4">
-        <div class="text-lg font-bold">Product Category</div>
-        <div class="text-gray-500">
-          Choose the category that aligns with your brand's current offerings or
-          explore new categories that you wish to pursue.
+        <div class="mt-4">
+          <div class="text-lg font-bold">Product Category</div>
+          <div class="text-gray-500">
+            Choose the category that aligns with your brand's current offerings
+            or explore new categories that you wish to pursue.
+          </div>
+          <n-select
+            v-model:value="selectedProductCategory"
+            :options="productTypeOptions"
+            placeholder="Select Category"
+            clearable
+          ></n-select>
         </div>
-        <n-select
-          v-model:value="selectedProductCategory"
-          :options="productTypeOptions"
-          placeholder="Select Category"
-          clearable
-        ></n-select>
-      </div>
 
-      <n-button
-        type="primary"
-        size="large"
-        class="my-10"
-        block
-        :disabled="!canGenerate"
-        @click="generateIdea"
-        >Generate Idea
-      </n-button>
+        <n-button
+          type="primary"
+          size="large"
+          class="my-10"
+          block
+          :disabled="!canGenerate"
+          @click="handleGenerateIdea"
+          >Generate Product Idea
+        </n-button>
+      </n-spin>
     </template>
-    <template v-else-if="fullPromptResponse && !loading">
-      <div class="text-3xl font-bold my-3">Your Product Idea is Ready!</div>
-      <div class="card typingEffect">
-        <div v-html="fullPromptResponse"></div>
-        <img
-          :src="pollinationUrl"
-          alt="Generating Image..."
-          class="rounded-lg px-8 py-5"
-        />
-      </div>
-
-      <n-button @click="reset" block type="primary" class="my-4"
-        >Generate a New Product Idea</n-button
-      >
-    </template>
-    <template v-else-if="loading">
-      <img :src="MrBeanWaiting" alt="" />
-      <div class="text-2xl font-bold">
-        We Appreciate Your Patience as We Prepare Your New Product Idea!
-      </div>
+    <template v-else>
+      <FinalResponse
+        :response="aiResponse"
+        :imageUrl="pollinationUrl"
+        @reset="reset"
+      />
     </template>
   </div>
 
@@ -110,11 +103,12 @@ import {
   NSwitch,
   NCollapseTransition,
   NGradientText,
+  NSpin,
 } from "naive-ui";
 import axios from "@/utils/axios.js";
 import CreateNewBrandModal from "./CreateNewBrandModal.vue";
 import BrandProductsInfo from "./BrandProductsInfo.vue";
-import MrBeanWaiting from "@/assets/gif/mr-bean-waiting.gif";
+import FinalResponse from "./FinalResponse.vue";
 
 const loading = ref(false);
 
@@ -247,11 +241,22 @@ const aiResponse = ref({
   uniqueSellingPoint: "",
 });
 
+function handleGenerateIdea() {
+  loading.value = true;
+
+  generateIdea()
+    .then(() => {
+      loading.value = false;
+    })
+    .catch(() => {
+      loading.value = false;
+    });
+}
+
 const generateIdea = async () => {
   try {
     // Prepare the data to send
 
-    loading.value = true;
     const requestData = {
       useBrandProfile: useBrandProfile.value,
       selectedProductCategory: selectedProductCategory.value,
@@ -274,10 +279,6 @@ const generateIdea = async () => {
       targetMarket: response.data.targetMarket,
       uniqueSellingPoint: response.data.uniqueSellingPoint,
     };
-
-    setTimeout(() => {
-      loading.value = false;
-    }, 10000);
   } catch (error) {
     alert(error);
   }
