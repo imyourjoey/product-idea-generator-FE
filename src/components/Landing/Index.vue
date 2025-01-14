7bf0e79091d739e56d037f8614ee58db4cc40e50
imyourjoey <joeylim016@gmail.com>
2025-01-14 14:18:20 +0800

    add credentials in landing page:

--- src/components/Landing/Index.vue
+++ src/components/Landing/Index.vue
@@ -24,7 +24,28 @@
           to Try Out Now!
         </div>
       </div>
+      <div class="mt-8 text-sm">
+        <div class="font-semibold">This is a demo website</div>
+        <div class="font-semibold">Use the credentials below to sign in</div>
+        <div class="text-gray-800">Username: tester@gmail.com</div>
+        <div class="text-gray-800">Password: password</div>
+      </div>
     </div>
+  </div>
+  <div
+    class="md:my-20 md:text-4xl text-3xl font-bold md:text-center mt-12 mb-3"
+  >
+    Watch a 1-minute video to get started!
+  </div>
+  <div class="relative w-full pb-[56.25%] overflow-hidden rounded-xl shadow-xl">
+    <iframe
+      class="absolute top-0 left-0 w-full h-full"
+      src="https://www.youtube.com/embed/--PAX3v9gpk?si=opLEXRmAdl25yXx7"
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
   </div>
 </template>
 
